709edc8e9f5d8ef5239666800e2cbe14a345b0ff
Espen Jones <epqr04@gmail.com>
2024-06-24 15:46:25 +0200

    Migrate to gulp v5

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,51 @@
-const gulp = require('gulp')
-const gulp_zip = require('gulp-zip')
-const fs = require('fs')
-const del = require('del')
-const pconf = require('./package.json')
+const gulp = require('gulp');
+const gulp_zip = require('gulp-zip');
+const pconf = require('./package.json');
 
 const conf = {
-    firefox: {
-        build: 'build/firefox/',
-        debug: 'debug/firefox/',
-    },
-    chrome: {
-        build: 'build/chrome/',
-        debug: 'debug/chrome/',
-    }
-}
+  firefox: {
+    build: 'build/firefox/',
+    debug: 'debug/firefox/',
+  },
+  chrome: {
+    build: 'build/chrome/',
+    debug: 'debug/chrome/',
+  }
+};
 
 // snakk om quick'n'dirty shit
 
-gulp.task('chrome:debug', () => {
-    gulp.src(['src/chrome/**', 'src/common/**', 'src/manifest.json'])
-        .pipe(gulp.dest(conf.chrome.debug))
-})
-gulp.task('chrome:build', () => {
-    gulp.src(['src/chrome/**', 'src/common/**', 'src/manifest.json'])
-        .pipe(gulp.dest(conf.chrome.build + 'tmp'))
-        .pipe(gulp_zip('chrome-' + pconf.version + '.zip'))
-        .pipe(gulp.dest(conf.chrome.build))
+function chromeDebug() {
+  return gulp.src(['src/chrome/**', 'src/common/**', 'src/manifest.json'])
+    .pipe(gulp.dest(conf.chrome.debug));
+}
+function chromeBuild() {
+  return gulp.src(['src/chrome/**', 'src/common/**', 'src/manifest.json'])
+    .pipe(gulp.dest(conf.chrome.build + 'tmp'))
+    .pipe(gulp_zip('chrome-' + pconf.version + '.zip'))
+    .pipe(gulp.dest(conf.chrome.build));
+}
 
-})
+function firefoxDebug() {
+  return gulp.src(['src/firefox/**', 'src/common/**', 'src/manifest.json'])
+    .pipe(gulp.dest(conf.firefox.debug));
+}
+function firefoxBuild() {
+  return gulp.src(['src/firefox/**', 'src/common/**', 'src/manifest.json'])
+    .pipe(gulp.dest(conf.firefox.build + 'tmp'))
+    .pipe(gulp_zip('firefox-' + pconf.version + '.zip'))
+    .pipe(gulp.dest(conf.firefox.build));
+}
 
-gulp.task('firefox:debug', () => {
-    gulp.src(['src/firefox/**', 'src/common/**', 'src/manifest.json'])
-        .pipe(gulp.dest(conf.firefox.debug))
-})
-gulp.task('firefox:build', () => {
-    gulp.src(['src/firefox/**', 'src/common/**', 'src/manifest.json'])
-        .pipe(gulp.dest(conf.firefox.build + 'tmp'))
-        .pipe(gulp_zip('firefox-' + pconf.version + '.zip'))
-        .pipe(gulp.dest(conf.firefox.build))
-})
+function debugWatch() {
+  gulp.watch('src/**', gulp.series(chromeDebug, firefoxDebug));
+}
 
-gulp.task('debug:watch', () => {
-    gulp.watch('src/**', ['chrome:debug', 'firefox:debug'])
-})
-gulp.task('default', ['chrome:debug', 'firefox:debug', 'chrome:build', 'firefox:build'])
+const build = gulp.parallel(chromeBuild, firefoxBuild);
+const debug = gulp.parallel(chromeDebug, firefoxDebug);
+const watch = gulp.series(debug, debugWatch);
+
+exports.build = build;
+exports.debug = debug;
+exports.watch = watch;
+exports.default = build;
